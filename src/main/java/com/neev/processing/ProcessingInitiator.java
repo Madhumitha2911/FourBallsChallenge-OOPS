4069da48f85117f2fc411a11054e13bc67cc841f
Madhumitha M <madhumitha.m@thoughtworks.com>
2021-05-23 11:39:07 +0530

    create four balls

--- src/main/java/com/neev/processing/ProcessingInitiator.java
+++ src/main/java/com/neev/processing/ProcessingInitiator.java
@@ -2,18 +2,30 @@ package com.neev.processing;
 
 import processing.core.PApplet;
 
+import java.util.List;
+
 public class ProcessingInitiator extends PApplet {
 
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
     public static final int NUMBER_OF_BALLS = 4;
+    private static  List<Ball> listOfBalls;
 
     public static void main(String[] args) {
         PApplet.main("com.neev.processing.ProcessingInitiator", args);
+        createBalls();
+    }
+
+    private static void createBalls() {
+        for (int ballNo = 0; ballNo < NUMBER_OF_BALLS; ballNo++) {
+            listOfBalls.add(new Ball(ballNo));
+        }
     }
 
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
     }
+
+
 }
