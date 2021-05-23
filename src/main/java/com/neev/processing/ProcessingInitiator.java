ce862e05a2170d2a3fdfac9661d484bdecb5a6b5
Madhumitha M <madhumitha.m@thoughtworks.com>
2021-05-23 11:07:43 +0530

    set screen size for creating sketch

---
+++ src/main/java/com/neev/processing/ProcessingInitiator.java
@@ -0,0 +1,18 @@
+package com.neev.processing;
+
+import processing.core.PApplet;
+
+public class ProcessingInitiator extends PApplet {
+
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+
+    public static void main(String[] args) {
+        PApplet.main("com.neev.processing.ProcessingInitiator",args);
+    }
+
+    @Override
+    public void settings() {
+        size(WIDTH, HEIGHT);
+    }
+}
